92f7c83e94c5ed378420a002184df728ca3165e5
Nidhi Praveen Jain <nidhiprj@usc.edu>
2014-09-10 01:51:59 -0700

    Delete q4.sql

--- HW3/q4.sql
+++
@@ -1,7 +0,0 @@
-select u.flight_num Flightnumber, x.agentName, x.phone
-from fltresv u,
-XMLTABLE('/requests/request'
-          passing u.request
-          columns agentName VARCHAR2(30) PATH 'agentName/text()',
-                  phone VARCHAR2(15) Path 'contactPerson/phone/text()') x
-where u.origin = 'Los Angeles';
