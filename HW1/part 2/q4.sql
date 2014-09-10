0dbad5a456be3019fe662583749f01ba6d36c027
Nidhi Praveen Jain <nidhiprj@usc.edu>
2014-09-10 01:53:34 -0700

    Delete q4.sql

--- HW1/part 2/q4.sql
+++
@@ -1,8 +0,0 @@
-select nickName
-from users u, private_message pm
-where u.userId = pm.userIdS and userIdR in
-(select userId
-from users
-where nickName like 'mickey')
-group by nickName
-having count(*)>2;
