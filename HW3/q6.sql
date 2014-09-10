0bfb8be6c8d86681d5bb87ccebc40187c38b7ca2
Nidhi Praveen Jain <nidhiprj@usc.edu>
2014-09-10 01:52:02 -0700

    Delete q6.sql

--- HW3/q6.sql
+++
@@ -1,8 +0,0 @@
-select to_char(x.travelDate,'dd-MON-rr') traveldate, sum(x.NumberOfSeats) NumberOfSeats
-from fltresv f,
-XMLTABLE('/requests/request/reservation'
-          passing f.request
-          columns travelDate timestamp PATH 'travelDate',
-          NumberOfSeats number PATH 'noOfSeats') x
-where f.flight_num = '215'
-group by x.travelDate
