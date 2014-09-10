7ba166c37d1e5e0511618c66e201cac436634c1e
Nidhi Praveen Jain <nidhiprj@usc.edu>
2014-09-10 01:53:40 -0700

    Delete q9.sql

--- HW1/part 2/q9.sql
+++
@@ -1,6 +0,0 @@
-select distinct userId, nickName, emailId
-from users 
-where userId in
-(select userId
-from public_message
-where dateTime between '01-JAN-2012 00:00:00' and '11-FEB-2013 23:59:59')
